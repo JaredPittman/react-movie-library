1a0f75dc39d91b53f2a88298f6a1fc1ad48cf983
JaredPittman <Jaredpittman0@gmail.com>
2023-01-19 14:46:01 -0330

    instructional span

--- src/Components/search.jsx
+++ src/Components/search.jsx
@@ -15,6 +15,7 @@ const Search = () => {
         placeholder="Enter movie keyword here"
       />
       <button onClick={handleSubmit}>Search</button>
+      <span>Enter a year you wish to see the movies from!</span>
     </div>
   );
 };
