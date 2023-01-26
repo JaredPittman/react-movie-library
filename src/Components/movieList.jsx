0a8665c29e82d475bccbb5818d10380af71935bd
JaredPittman <Jaredpittman0@gmail.com>
2023-01-26 09:19:12 -0330

    adding chakra ui to display grid style

--- src/Components/movieList.jsx
+++ src/Components/movieList.jsx
@@ -1,22 +1,48 @@
 import React from "react";
 import { useContext } from "react";
 import { GlobalContext } from "../GlobalContext";
+// import { inputAnatomy } from "@chakra-ui/anatomy";
+import { Text, Image, Container, SimpleGrid, Box } from "@chakra-ui/react";
 
 const MovieList = () => {
   const { movieList } = useContext(GlobalContext);
   console.log(movieList);
   return (
-    <div>
+    <Container>
       {movieList && movieList.length > 0
         ? movieList.map((item) => (
-            <div key={item.Year}>
-              <img src={item.Poster} alt="Movie Poster" />
-              <p>{item.Title}</p>
-              <p>{item.Year}</p>
-            </div>
+            <SimpleGrid
+              templateColumns="repeat(3, 1fr)"
+              templateRows="repeat(1 ,1fr)"
+              gap="2"
+              p="10px"
+              spacing={"10"}
+            >
+              <Box
+                b="100%"
+                h="100%"
+                key={item.Year}
+                bgColor={"gray.400"}
+                borderRadius={"15"}
+                colSpan={1}
+                rowSpan={1}
+              >
+                <Container>
+                  <Image
+                    p={"10px"}
+                    src={item.Poster}
+                    alt="Missing Movie Poster"
+                    fontWeight={"bold"}
+                    color={"red"}
+                  />
+                  <Text>{item.Title}</Text>
+                  <Text>{item.Year}</Text>
+                </Container>
+              </Box>
+            </SimpleGrid>
           ))
         : null}
-    </div>
+    </Container>
   );
 };
 
